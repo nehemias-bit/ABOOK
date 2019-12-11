bca7646cc1ba7459dce8dda09356078d1bf861ae
Nehemias Cruz <nehemiascruz@Nehemiass-MacBook-Pro.local>
2019-12-11 09:27:15 -0500

    surge

--- client/src/services/api-helper.js
+++ client/src/services/api-helper.js
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
-// const baseUrl = "https://abook-api.herokuapp.com/"
+const baseUrl = "https://abook-api.herokuapp.com"
 
-const baseUrl = 'http://localhost:3000'
+// const baseUrl = 'http://localhost:3000'
 
 const api = axios.create({
   baseURL: baseUrl
